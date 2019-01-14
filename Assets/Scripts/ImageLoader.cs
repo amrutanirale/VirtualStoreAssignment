a9286525fc2e469268f8c616b5cff0e08c5dc22e
Amruta Nirale <nirale.amruta@gmail.com>
2019-01-14 19:28:20 +0530

    Initial commit

---
+++ Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.IO;
+using UnityEditor;
+
+public class ImageLoader : MonoBehaviour
+{
+    string path;
+    public RawImage image;
+    public GameObject cube;
+
+    public void OpenFileExplorer()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+        GetImage();
+    }
+
+
+    void GetImage()
+    {
+        if (path != null)
+        {
+            UpdateImage();
+        }
+    }
+
+    void UpdateImage()
+    {
+        WWW www = new WWW("file:///" + path);
+        image.texture = www.texture;
+        cube.GetComponent<MeshRenderer>().material.mainTexture = www.texture;
+
+    }
+
+}
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    bool isClicked = false;
+    [SerializeField]
+    private float rotationSpeed = 1f;
+
+    void OnMouseDown()
+    {
+        isClicked = true;
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+
+    }
+    private void OnMouseUp()
+    {
+        isClicked = false;
+    }
+    void OnMouseDrag()
+    {
+        Vector3 cursorScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint) + offset;
+        transform.position = cursorPosition;
+    }
+
+    private void Update()
+    {
+        if (isClicked == true)
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                print("r");
+                transform.Rotate(Vector3.up * rotationSpeed);
+            }
+
+            if (Input.GetKey(KeyCode.T))
+            {
+                print("t");
+                transform.Rotate(Vector3.down * rotationSpeed);
+            }
+        }
+    }
+}
