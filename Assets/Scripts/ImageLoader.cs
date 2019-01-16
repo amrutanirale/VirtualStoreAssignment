c6738fee72dc2b042c1927cd53aed1cb10cf25b3
krayonik <amruta@krayonik.com>
2019-01-16 16:08:28 +0530

    Implemented Open File System for WebGL

--- Assets/Scripts/ImageLoader.cs
+++ Assets/Scripts/ImageLoader.cs
@@ -1,41 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System;
 using System.IO;
 using UnityEditor;
-using Crosstales.FB;
-//using SFB;
+using SFB;
+using UnityEngine.EventSystems;
+[RequireComponent( typeof( Button ) )]
 public class ImageLoader : MonoBehaviour
 {
     string path;
-    //public RawImage image;
     public GameObject productPrefab;
     public GameObject player;
 
-
     public void OpenFileExplorer()
     {
-       // path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
-       // path = StandaloneFileBrowser.OpenFilePanel("Title", "", ".png", false);
-        //System.Diagnostics.Process.Start("explorer.exe", "/select," + path);
-
-        //if (path.Length > 0)
-        //{
-        //    StartCoroutine(OutputRoutine(new System.Uri(path[0]).AbsolutePath));
-        //}
-
-        path=FileBrowser.OpenSingleFile("Open File", "" , ".png");
+#if UNITY_EDITOR
+        path = EditorUtility.OpenFilePanel( "Overwrite with png" , "" , "png" );
         GetImage();
+
+#endif
+         var extensions = new[] 
+        {
+            new ExtensionFilter("Image Files", "png" ),
+           
+        };
+        var paths = StandaloneFileBrowser.OpenFilePanel("Title", "", extensions, true);
+        if (paths.Length > 0) 
+        {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        }
+
     }
 
-    private IEnumerator OutputRoutine(string url)
-    {
-        var loader = new WWW(url);
-        yield return loader;
-        productPrefab.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = loader.texture;
-        Instantiate(productPrefab, player.transform.position + (player.transform.forward * 2), player.transform.rotation);
-    }
     void GetImage()
     {
         if (path != null)
@@ -43,14 +42,22 @@ public class ImageLoader : MonoBehaviour
             UpdateImage();
         }
     }
+    
 
     void UpdateImage()
     {
         WWW www = new WWW("file:///" + path);
-        //image.texture = www.texture;
-        productPrefab.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = www.texture;
-        Instantiate(productPrefab, player.transform.position + (player.transform.forward * 2), player.transform.rotation);
-        
+        GameObject newProduct= Instantiate( productPrefab , player.transform.position + ( player.transform.forward * 2 ) , player.transform.rotation );
+        newProduct.GetComponent<Renderer>().material.mainTexture = www.texture;
     }
 
+    private IEnumerator OutputRoutine( string url )
+    {
+        var loader = new WWW( url );
+        yield return loader;
+        GameObject newProduct = Instantiate( productPrefab , player.transform.position + ( player.transform.forward * 2 ) , player.transform.rotation );
+
+        newProduct.GetComponent<Renderer>().material.mainTexture = loader.texture;
+    }
+   
 }
---
+++ Assets/StandaloneFileBrowser/Sample/CanvasSampleOpenFileImage.cs
@@ -0,0 +1,64 @@
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using SFB;
+
+[RequireComponent(typeof(Button))]
+public class CanvasSampleOpenFileImage : MonoBehaviour, IPointerDownHandler {
+    //public RawImage output;
+    public GameObject productPrefab;
+    public GameObject player;
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    //
+    // WebGL
+    //
+    [DllImport("__Internal")]
+    private static extern void UploadFile(string gameObjectName, string methodName, string filter, bool multiple);
+
+    public void OnPointerDown(PointerEventData eventData) 
+    {
+        UploadFile(gameObject.name, "OnFileUpload", ".png", false);
+    }
+
+    // Called from browser
+    public void OnFileUpload(string url) {
+        StartCoroutine(OutputRoutine(url));
+    }
+#else
+    //
+    // Standalone platforms & editor
+    //
+    public void OnPointerDown(PointerEventData eventData) { }
+
+    void Start() {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick() {
+
+        var extensions = new[]
+       {
+            new ExtensionFilter("Image Files", "png" ),
+
+        };
+        var paths = StandaloneFileBrowser.OpenFilePanel("Title", "", extensions, true);
+        if (paths.Length > 0) {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        }
+    }
+#endif
+
+    private IEnumerator OutputRoutine(string url) {
+        var loader = new WWW(url);
+        yield return loader;
+        GameObject newProduct = Instantiate( productPrefab , player.transform.position + ( player.transform.forward * 2 ) , player.transform.rotation );
+
+        newProduct.GetComponent<Renderer>().material.mainTexture = loader.texture;
+    }
+}
